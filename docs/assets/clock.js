a303cecf327bf6f35bc27eccd82a8dabc6b25ee9
Bryant Kadenge <deshjuniour@gmail.com>
2021-05-03 09:49:40 +0300

    Update clock.js

--- docs/assets/clock.js
+++ docs/assets/clock.js
@@ -6,7 +6,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Dec 24, 2018 00:00:00').getTime(),
+let countDown = new Date('May 1, 2021 00:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
